dabaf99dc481eccc178fb780f54077178f0a9d35
Valeria <valberroc@alu.edu.gva.es>
2026-05-08 09:47:46 +0200

    "tio mis modelos :("

--- data/addons/dronify/__manifest__.py
+++ data/addons/dronify/__manifest__.py
@@ -1,21 +1,21 @@
 {
-    'name': "dronify",
+    'name': "Dronify",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "Módulo personalizado de Odoo para la gestión de Dronify",
 
     'description': """
 Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Valeria Berrutti",
+    'website': "https://www.dronify.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
-
+    'application': True,
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
